eddccbb83bbb812473eff9c3771caa5d0d497a6b
Shashikant Awasthi <93568449+bb-shashikant@users.noreply.github.com>
2022-01-28 15:30:59 +0530

    Update Constants.java

--- src/main/java/files/Constants.java
+++ src/main/java/files/Constants.java
@@ -82,9 +82,9 @@ public class Constants {
     public static String BBADMINAUTHTOKEN;
     public static void setBBADMINAUTHTOKEN(){
         if(env_type.equals("QAS")){
-            BBADMINAUTHTOKEN="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJjaGFmZiI6IldCV0RSb3VXMVU4TEJRIiwidWlkIjoyMDg1OTUsInRpbWUiOjE2Mzk3NDY1NjkuMDUxNDYzfQ.AxUTloSxRNm168egg5RQnTDiSbmAH0YZLOGcM9hJiAk";
+            BBADMINAUTHTOKEN="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJjaGFmZiI6IldCV0RSb3VXMVU4TEJRIiwidWlkIjoyMDg1OTUsInRpbWUiOjE2Mzk3NDY1NjkuMDUxNDYzfQ.AxUTloSxRNm168egg5RQnTDiSbmAH0YZLOGcM9hJiA";
         }
-        else BBADMINAUTHTOKEN="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJjaGFmZiI6IkF6WGRlLVFUNHdid0FBIiwidWlkIjoyMTAwMzYsInRpbWUiOjE2NDA2NzY4MzcuODAxOTk1fQ.K9JfDVDu-wk5TH08_biYkdE0fsdGCM-uOZwinW0PZkU";
+        else BBADMINAUTHTOKEN="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJjaGFmZiI6IkF6WGRlLVFUNHdid0FBIiwidWlkIjoyMTAwMzYsInRpbWUiOjE2NDA2NzY4MzcuODAxOTk1fQ.K9JfDVDu-wk5TH08_biYkdE0fsdGCM-uOZwinW0PZk";
     }
     public static String getBBADMINAUTHTOKEN(){
         return BBADMINAUTHTOKEN;
